d809b48c87341234c3f5e33a18ece867b9a43f62
Vasundhara Ravi <vasundhararavi641@gmail.com>
2026-06-30 11:52:12 +0200

    feat: trying to fix geo board issues

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -34,7 +34,7 @@ export default {
       },
       "config": {
         "googleMaps": {
-          "apiKey": process.env.GOOGLE_MAPS_API_KEY,
+          "apiKey": process.env.GOOGLE_MAPS_API_KEY
         }
       },
       "permissions": [
